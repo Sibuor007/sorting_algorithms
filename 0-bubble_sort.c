727bce08cdcddef2561c754381235c6d84f450bc
Sibuor007 <opolotieno851@gmail.com>
2024-01-23 06:42:43 -0800

    updates

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,49 +1,40 @@
 #include "sort.h"
 
 /**
- * swap_nums - func to swap two numbers in an array.
- * @var_0: The first number to swap.
- * @var_1: The second number to swap.
- */
-void swap_nums(int *var_0, int *var_1)
-{
-	int temp;
-
-	temp = *var_0;
-	*var_0 = *var_1;
-	*var_1 = temp;
-}
-
-/**
  * bubble_sort - func to sort an array in ascending order.
  * @array: An array of numbers to sort.
  * @size: The size of the array.
  * Description: Prints the array after every swap
  */
+
 void bubbleSort(int *array, size_t size)
 {
-	size_t item, length = size;
-	bool flag= false;
+	size_t j, length = size;
+	int k, temp;
+	int flag;
 
 	if (array == NULL || size < 2)
 		return;
 
-	do
+	for (j = 0; j < length - 1; j++)
 	{
-		flag = false;
-		for (item = 0; item < length - 1; item++)
+		flag = 0;
+		for (k = 0; k < length - 1 - j; k++)
 		{
-			if (array[item] > array[item + 1])
+
+			if (array[k] > array[k + 1])
 			{
 
-				int temp = array[item];
-				array[item] = array[item + 1];
-				array[item + 1] = temp;
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
 
 				print_array(array, size);
-				flag = true;
+				flag = 1;
 			}
 		}
 		length--;
-	} while (flag);
+		if (flag == 0)
+			break;
+	}
 }
